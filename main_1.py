03a91ef788ec376c0b1e7f41995508d384696e83
mano556 <mk391982@gmail.com>
2024-01-02 13:10:19 +0530

    files changed

--- main_1.py
+++ main_1.py
@@ -1,66 +1,66 @@
 # # Print two tables
 
-# for x in range (1,11):
-#     print(x,"x 2 =",x*9)
+for x in range (1,11):
+    print(x,"x 2 =",x*9)
 
 # # print count of even numbers <Method-1>
-# count=[]
-# for x in range(1,11):
-#     if (x%2 ==0):
-#         count.append(x)
-# print(len(count))
+count=[]
+for x in range(1,11):
+    if (x%2 ==0):
+        count.append(x)
+print(len(count))
 
 # # print count of even  & even numbers <Method-2>
-# even_count=0
-# odd_count =0
-# for x in range(1,11):
-#     if (x%2==0):
-#         even_count+=1
-#     else:
-#         odd_count+=1
-# print("Even : ",even_count)
-# print("Odd : ",odd_count)
+even_count=0
+odd_count =0
+for x in range(1,11):
+    if (x%2==0):
+        even_count+=1
+    else:
+        odd_count+=1
+print("Even : ",even_count)
+print("Odd : ",odd_count)
 
 # # print count of numbers which are divisible by 3 &5
-# count_3 = 0
-# count_5 = 0
-# count_3and5 = 0
+count_3 = 0
+count_5 = 0
+count_3and5 = 0
 
-# for x in range(1, 101):
-#     if (x % 3 == 0) and (x % 5 == 0):
-#         count_3and5 += 1
-#     elif (x % 3 == 0):
-#         count_3 += 1
-#     elif (x % 5 == 0):
-#         count_5 += 1
+for x in range(1, 101):
+    if (x % 3 == 0) and (x % 5 == 0):
+        count_3and5 += 1
+    elif (x % 3 == 0):
+        count_3 += 1
+    elif (x % 5 == 0):
+        count_5 += 1
 
-# print("3 divisible : ", count_3)
-# print("5 divisible : ", count_5)
-# print("Both 3 & 5 divisible : ", count_3and5)
+print("3 divisible : ", count_3)
+print("5 divisible : ", count_5)
+print("Both 3 & 5 divisible : ", count_3and5)
 
 # # print sum of natural numbers from 1 to 5
-# sum=0
-# for x in range(1,6):
-#     sum+=x
-# print(sum)
+sum=0
+for x in range(1,6):
+    sum+=x
+print(sum)
 
 # # ask 10 input from user then sum then give average of inputs
-# a=[]
-# sum=0
-# for x in range(10):
-#     num=int(input("Enter the value of " + str(x+1)+" : "))
-#     a.append(num)
-# for i in a:
-#     sum+=i
-# result=sum/len(a)
-# print(result)
+a=[]
+sum=0
+for x in range(10):
+    num=int(input("Enter the value of " + str(x+1)+" : "))
+    a.append(num)
+for i in a:
+    sum+=i
+result=sum/len(a)
+print(result)
 
 # first 7 natural number
 
-# num=int(input("Enter the number : "))
-# print("first "+str(num)+" natural numbers : ")
-# for x in range(1,num+1):
-#    print(x)
+num=int(input("Enter the number : "))
+print("first "+str(num)+" natural numbers : ")
+for x in range(1,num+1):
+   print(x)
     
 # cube of an integer
 num=int(input("Enter the number : "))
--- swap.py
+++ swap.py
@@ -1,22 +1,22 @@
 # swap two variable with temp
-a=77
-b=12
-temp=a
+# a=77
+# b=12
+# temp=a
 
-a=b
-b=temp
-print("value of a is ",a)
-print("value of b is ",b)
+# a=b
+# b=temp
+# print("value of a is ",a)
+# print("value of b is ",b)
 
 #  swap two variable without temp using add sub
-a=int(input("Enter a value for a : "))
-b=int(input("Enter a value for b : "))
-a=a+b
-b=a-b
-a=a-b
+# a=int(input("Enter a value for a : "))
+# b=int(input("Enter a value for b : "))
+# a=a+b
+# b=a-b
+# a=a-b
 
-print("value of a is ",a)
-print("value of b is ",b)
+# print("value of a is ",a)
+# print("value of b is ",b)
 
 #  swap two variable without temp using xor operator
 a=int(input("Enter a value for a : "))
@@ -27,6 +27,5 @@ a=a^b
 
 print("value of a is ",a)
 print("value of b is ",b)
-
 
 
